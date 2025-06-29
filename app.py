de419fa97641e102dcd38d11ecfda468beb1e2cc
Agnel-Raj <agnelraj1988@gmai.com>
2025-06-30 01:15:26 +0530

    Change port

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
 from flask import Flask, request, render_template
 from model import SentimentRecommenderModel
 
-
 app = Flask(__name__)
-
 sentiment_model = SentimentRecommenderModel()
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/predict', methods=['POST'])
 def prediction():
-    # get user from the html form
-    user = request.form['userName']
-    # convert text to lowercase
-    user = user.lower()
+    user = request.form['userName'].lower()
     items = sentiment_model.getSentimentRecommendations(user)
 
-    if(items is not None):
-        print(f"retrieving items....{len(items)}")
-        print(items)
-        # data=[items.to_html(classes="table-striped table-hover", header="true",index=False)
-        return render_template("index.html", column_names=items.columns.values, row_data=list(items.values.tolist()), zip=zip,user=user)
+    if items is not None:
+        return render_template("index.html", column_names=items.columns.values, row_data=list(items.values.tolist()), zip=zip, user=user)
     else:
-        return render_template("index.html", message="User Name doesn't exists, No product recommendations at this point of time!")
-
+        return render_template("index.html", message="User Name doesn't exist. No product recommendations at this point of time!")
 
 @app.route('/predictSentiment', methods=['POST'])
 def predict_sentiment():
-    # get the review text from the html form
     review_text = request.form["reviewText"]
-    print(review_text)
     pred_sentiment = sentiment_model.classify_sentiment(review_text)
-    print(pred_sentiment)
     return render_template("index.html", sentiment=pred_sentiment)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
